f854aeb241b8bb27e751375b6fa5eeaeadcd6160
Andrew Cho <cyh76507707@gmail.com>
2016-02-23 18:13:44 +0900

    added collection view

---
+++ pokedex/pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  pokemon.swift
+//  pokedex
+//
+//  Created by Young-HwiCho on 2/23/16.
+//  Copyright © 2016 YHCho. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
